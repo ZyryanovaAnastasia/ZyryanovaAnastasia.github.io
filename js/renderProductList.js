082080f4bf89c4b1d3aacd1232e739acdcec430b
Zyryanova <azyiryanova@sdvor.com>
2021-10-03 22:08:58 +0500

    урок два. Добавила классы

--- js/renderProductList.js
+++ js/renderProductList.js
@@ -1,27 +1,59 @@
-    const products = [
-        { title: 'pants', price: 150 },
-        { title: 'socks', price: 50 },
-        { title: 'jacket', price: 350 },
-        { title: 'dress', price: 250 },
-    ];
+    class ProductItem {
+        constructor(title= 'Название', price= 0) {
+            this.title = title;
+            this.price = price;
+        }
 
-    const renderProductItem = (title = "Название", price = 0) => {
-        const productItem = `
-        <div class="product_list-item">
-        <img src="img/${title}.jpg" alt="img${title}" class="product_list-item-img">
-        <div class="product_list-item-div_text">
-            <h3 class="product_list-item-title">${title}</h3>
-            <span class="product_list-item-price">${price}руб.</span>
-        </div>
-        <button class="product_list-item-btn_add">Добавить</button>
-        </div>
-        `;
-        return productItem;
-    };
-
-    const renderProductList = (list) => {
-        let productList = list.map(({title, price}) => renderProductItem(title, price));
-        document.querySelector('.product_list').innerHTML = productList.join('');
+        render() { 
+            return`
+                <div class="product_list-item">
+                <img src="img/${this.title}.jpg" alt="img${this.title}" class="product_list-item-img">
+                <div class="product_list-item-div_text">
+                    <h3 class="product_list-item-title">${this.title}</h3>
+                    <span class="product_list-item-price">${this.price}руб.</span>
+                </div>
+                <button class="product_list-item-btn_add">Добавить</button>
+                </div>
+            `;
+        }
     }
 
-    renderProductList(products);
+    class ProductList {
+        products = [];
+
+        constructor() {
+            this.fetchProducts();
+            this.render();
+            this.getSumPrice();
+        }
+
+        fetchProducts() {
+            this.products = [
+                { title: 'pants', price: 150 },
+                { title: 'socks', price: 50 },
+                { title: 'jacket', price: 350 },
+                { title: 'dress', price: 250 },
+            ];
+        }
+
+        getSumPrice() {
+            let sum = 0;
+            for (let product of this.products) {
+                sum += product.price;
+            }
+
+            return sum;
+        }
+
+        render() { 
+            const productList = this.products.map(({title, price}) => {
+                const productItem = new ProductItem(title, price);
+                return productItem.render();
+            });
+            document.querySelector('.product_list').innerHTML = productList.join('');
+        }
+    }
+
+    onload = () => {
+        new ProductList();
+    }
