9e583c4fdd7a682b96639652afd70db3dde872cd
Zyryanova <azyiryanova@sdvor.com>
2021-10-10 01:38:44 +0500

    lesson3. Добавила промис

--- js/renderProductList.js
+++ js/renderProductList.js
@@ -1,59 +1,75 @@
-    class ProductItem {
-        constructor(title= 'Название', price= 0) {
-            this.title = title;
-            this.price = price;
-        }
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const CATALOG_URL = '/catalogData.json';
 
-        render() { 
-            return`
-                <div class="product_list-item">
-                <img src="img/${this.title}.jpg" alt="img${this.title}" class="product_list-item-img">
-                <div class="product_list-item-div_text">
-                    <h3 class="product_list-item-title">${this.title}</h3>
-                    <span class="product_list-item-price">${this.price}руб.</span>
-                </div>
-                <button class="product_list-item-btn_add">Добавить</button>
-                </div>
-            `;
+function makeGETRequest(method, url) {
+    return new Promise((resolve, reject) => {
+        var xhr;
+    
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { 
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
         }
+    
+        xhr.open(method, `${API_URL}${url}`, true);
+        xhr.send();
+        xhr.onload = () => {
+            resolve(this.products = JSON.parse(xhr.responseText))
+        };
+    });
+}
+
+class ProductItem {
+    constructor(product_name = 'Название', price = 0) {
+        this.product_name = product_name;
+        this.price = price;
     }
 
-    class ProductList {
-        products = [];
+    render() { 
+        return`
+            <div class="product_list-item">
+            <img src="img/${this.product_name}.jpg" alt="img${this.product_name}" class="product_list-item-img">
+            <div class="product_list-item-div_text">
+                <h3 class="product_list-item-title">${this.product_name}</h3>
+                <span class="product_list-item-price">${this.price}руб.</span>
+            </div>
+            <button class="product_list-item-btn_add">Добавить</button>
+            </div>
+        `;
+    }
+}
 
-        constructor() {
-            this.fetchProducts();
+class ProductList {
+    products = [];
+
+    constructor() {
+        this.fetchProducts().then(() => {
             this.render();
             this.getSumPrice();
-        }
+        });
 
-        fetchProducts() {
-            this.products = [
-                { title: 'pants', price: 150 },
-                { title: 'socks', price: 50 },
-                { title: 'jacket', price: 350 },
-                { title: 'dress', price: 250 },
-            ];
-        }
-
-        getSumPrice() {
-            let sum = 0;
-            for (let product of this.products) {
-                sum += product.price;
-            }
-
-            return sum;
-        }
-
-        render() { 
-            const productList = this.products.map(({title, price}) => {
-                const productItem = new ProductItem(title, price);
-                return productItem.render();
-            });
-            document.querySelector('.product_list').innerHTML = productList.join('');
-        }
     }
 
-    onload = () => {
-        new ProductList();
+    fetchProducts() {
+        return makeGETRequest('GET', CATALOG_URL).then(productsList => {
+            this.products = productsList;
+        });
     }
+
+    getSumPrice() {
+        console.log(this.products.reduce((prev, {price}) => prev + price, 0))
+        return this.products.reduce((prev, {price}) => prev + price, 0);
+    }
+
+    render() { 
+        const productList = this.products.map(({product_name, price}) => {
+            const productItem = new ProductItem(product_name, price);
+            return productItem.render();
+        });
+        document.querySelector('.product_list').innerHTML = productList.join('');
+    }      
+}
+
+window.onload = () => {
+    new ProductList();
+}
